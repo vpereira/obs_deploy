53c30f7694580ddd77cb9359672d2f056dd630ab
Victor Pereira <vpereira@suse.de>
2020-03-31 12:05:07 +0200

    Move CheckDiff class to its own file

--- lib/obs_deploy.rb
+++ lib/obs_deploy.rb
@@ -1,49 +1,13 @@
 # frozen_string_literal: true
 
-require 'obs_deploy/version'
 require 'open-uri'
 require 'net/http'
 require 'nokogiri'
+require 'obs_deploy/version'
+require 'obs_deploy/check_diff'
 
 module ObsDeploy
   class Error < StandardError; end
-
-  class CheckDiff
-    def initialize(server: 'https://api.opensuse.org', product: 'SLE_12_SP4')
-      @server = server
-      @product = product
-    end
-
-    def package_version
-      doc = Nokogiri::XML(open(package_url))
-      doc.xpath("//binary[starts-with(@filename, 'obs-api')]/@filename").to_s
-    end
-
-    def package_commit
-      package_version.match(/obs-api-.*\..*\..*\.(.*)-.*\.rpm/).captures.first
-    end
-
-    def obs_running_commit
-      doc = Nokogiri::XML(about_url)
-      doc.xpath('//commit/text()').to_s
-    end
-
-    def github_diff
-      Net::HTTP.get(URI("https://github.com/openSUSE/open-build-service/compare/#{obs_running_commit}...#{package_commit}.diff"))
-    end
-
-    def has_migration?
-      !!github_diff.match(%r{db/migrate})
-    end
-
-    def package_url
-      URI("#{@server}/public/build/OBS:Server:Unstable/#{@product}/x86_64/obs-server")
-    end
-
-    def about_url
-      URI("#{@server}/about")
-    end
-  end
   class Systemctl
     class << self
       def status
---
+++ lib/obs_deploy/check_diff.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module ObsDeploy
+  class CheckDiff
+    def initialize(server: 'https://api.opensuse.org', product: 'SLE_12_SP4')
+      @server = server
+      @product = product
+    end
+
+    def package_version
+      doc = Nokogiri::XML(open(package_url))
+      doc.xpath("//binary[starts-with(@filename, 'obs-api')]/@filename").to_s
+    end
+
+    def package_commit
+      package_version.match(/obs-api-.*\..*\..*\.(.*)-.*\.rpm/).captures.first
+    end
+
+    def obs_running_commit
+      doc = Nokogiri::XML(about_url)
+      doc.xpath('//commit/text()').to_s
+    end
+
+    def github_diff
+      Net::HTTP.get(URI("https://github.com/openSUSE/open-build-service/compare/#{obs_running_commit}...#{package_commit}.diff"))
+    end
+
+    def has_migration?
+      return true if github_diff.nil? || github_diff.empty?
+
+      !!github_diff.match(%r{db/migrate})
+    end
+
+    def package_url
+      URI("#{@server}/public/build/OBS:Server:Unstable/#{@product}/x86_64/obs-server")
+    end
+
+    def about_url
+      URI("#{@server}/about")
+    end
+  end
+end
