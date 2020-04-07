a74e311a84cd38797358d485b9b2d55dddd01440
Victor Pereira <vpereira@suse.de>
2020-04-07 10:57:25 +0200

    Refactor ssh class

--- lib/obs_deploy/ssh.rb
+++ lib/obs_deploy/ssh.rb
@@ -13,13 +13,34 @@ module ObsDeploy
     end
 
     def build_command
-      command_line = ['-tt', "#{@user}@#{@server}", '-p', @port.to_s]
-      command_line << ['-i', @identity_file] if @identity_file
-      ['ssh'] + command_line.flatten
+      ['ssh'] + basic_connection_string + identity + ssh_port
     end
 
     def run(cmd)
       Cheetah.run(build_command + cmd, logger: logger)
+    end
+
+    private
+
+    # -t Force pseudo-tty allocation.
+    # This can be used to execute arbitrary screen-based programs on a
+    # remote machine, which can be very useful, e.g. when implementing menu services.
+    # Multiple -t
+    # options force tty allocation, even if ssh has no local tty.
+    def ssh_tt
+      ['-tt']
+    end
+
+    def identity
+      @identity_file ? ['-i', @identity_file] : []
+    end
+
+    def basic_connection_string
+      ssh_tt + [[@user, '@', @server].join]
+    end
+
+    def ssh_port
+      @port && @port != 22 ? ['-p', @port.to_s] : []
     end
 
     def logger
--- spec/ssh_spec.rb
+++ spec/ssh_spec.rb
@@ -5,10 +5,21 @@ RSpec.describe ObsDeploy::SSH do
     context 'default parameters' do
       subject { described_class.new }
 
-      it { expect(subject.port).to eq(22) }
       it { expect(subject.user).to eq('root') }
       it { expect(subject.server).to eq('localhost') }
-      it { expect(subject.build_command).to eq(['ssh', '-tt', 'root@localhost', '-p', '22']) }
+      it { expect(subject.build_command).to eq(['ssh', '-tt', 'root@localhost']) }
+    end
+
+    context 'passing port 22' do
+      subject { described_class.new(port: 22) }
+      it { expect(subject.build_command).to eq(['ssh', '-tt', 'root@localhost']) }
+    end
+
+    context 'passing custom port' do
+      subject { described_class.new(port: 2222) }
+
+      it { expect(subject.port).to eq(2222) }
+      it { expect(subject.build_command).to eq(['ssh', '-tt', 'root@localhost', '-p', '2222']) }
     end
   end
 end
